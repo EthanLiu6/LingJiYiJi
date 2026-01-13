427fb5e4269ec71dc4bc14bfa0468f891f2122f8
EthanLiu6 <liue2336@gmail.com>
2026-01-13 19:22:35 +0800

    fix: 后台不退出

--- LingJiYiJi/LingJiYiJiApp.swift
+++ LingJiYiJi/LingJiYiJiApp.swift
@@ -42,4 +42,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return false
     }
+    
+    func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+        if !flag {
+            for window in sender.windows {
+                window.makeKeyAndOrderFront(nil)
+            }
+        }
+        return true
+    }
 }
